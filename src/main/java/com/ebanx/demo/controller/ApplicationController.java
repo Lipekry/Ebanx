ed7abc88661dbdc6d39bf06c7756f1bc10d63bee
Luis Kryzozun <luis.kryzozun@rpinfo.com.br>
2026-02-15 22:44:09 -0300

    Feat: Adiciona retorno padrão

--- src/main/java/com/ebanx/demo/controller/ApplicationController.java
+++ src/main/java/com/ebanx/demo/controller/ApplicationController.java
@@ -1,11 +1,17 @@
 package com.ebanx.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Ebanx";
+    }
 
     @PostMapping("reset")
     public ResponseEntity<?> resetDataBase() {
