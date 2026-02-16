ec2a4730bdfd06fbb31c2abe7abbaf5e771d6ad6
Luis Kryzozun <luis.kryzozun@rpinfo.com.br>
2026-02-16 20:15:22 -0300

    Feat: Implementa service relacionado a conta

--- src/main/java/com/ebanx/demo/controller/AccountController.java
+++ src/main/java/com/ebanx/demo/controller/AccountController.java
@@ -1,11 +1,16 @@
 package com.ebanx.demo.controller;
 
+import com.ebanx.demo.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+
+    @Autowired
+    AccountService accountService;
 
     @GetMapping("/balance")
     public ResponseEntity<Integer> balance() {
