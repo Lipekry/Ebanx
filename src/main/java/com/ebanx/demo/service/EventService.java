dd1e87de33a520223d480349be87c79058f35912
Luis Kryzozun <luis.kryzozun@rpinfo.com.br>
2026-02-16 23:00:31 -0300

    Feat: Implementa service de eventos para controle de transações.

--- src/main/java/com/ebanx/demo/service/EventService.java
+++ src/main/java/com/ebanx/demo/service/EventService.java
@@ -1,6 +1,13 @@
 package com.ebanx.demo.service;
 
+import com.ebanx.demo.model.dto.AccountDto;
+import com.ebanx.demo.model.dto.EventRequestDto;
+import com.ebanx.demo.model.dto.EventResponseDto;
+import com.ebanx.demo.model.entity.Account;
+import com.ebanx.demo.repository.implementations.AccountRepositoryInMemory;
 import com.ebanx.demo.repository.interfaces.AccountRepository;
+import com.ebanx.demo.service.factory.EventStrategyFactory;
+import com.ebanx.demo.service.strategy.interfaces.EventStrategy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +15,36 @@ import org.springframework.stereotype.Service;
 public class EventService {
 
     private AccountRepository accountRepository;
+    private EventStrategyFactory factory;
 
-    public EventService(@Qualifier("inMemory") AccountRepository accountRepository) {
+    public EventService(@Qualifier("inMemory") AccountRepository accountRepository, EventStrategyFactory factory) {
         this.accountRepository = accountRepository;
+        this.factory = factory;
+    }
+
+    private EventResponseDto reponseHandler(EventRequestDto eventRequestDto) {
+        AccountDto accountOrigin = null;
+        AccountDto accountDestination = null;
+
+        if (eventRequestDto.getOrigin() != null && eventRequestDto.getOrigin() > 0) {
+            Account origin = this.accountRepository.getAccountById(eventRequestDto.getOrigin());
+            accountOrigin = new AccountDto();
+            accountOrigin.assign(origin);
+        }
+
+        if (eventRequestDto.getDestination() != null && eventRequestDto.getDestination() > 0) {
+            Account destination = this.accountRepository.getAccountById(eventRequestDto.getDestination());
+            accountDestination = new AccountDto();
+            accountDestination.assign(destination);
+        }
+
+        return new EventResponseDto(accountOrigin, accountDestination);
+    }
+
+    public EventResponseDto process(EventRequestDto request) throws Exception {
+        EventStrategy strategy =  this.factory.getStrategy(request.getType());
+        strategy.execute(request);
+        return reponseHandler(request);
     }
 
 }
