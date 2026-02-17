29baa4c78d43772e1eaa29e0cdadf0cd88f9bb04
Luis Kryzozun <luis.kryzozun@rpinfo.com.br>
2026-02-16 23:01:09 -0300

    Refactor: Realiza chamada correta para métodos do Service.

--- src/main/java/com/ebanx/demo/controller/EventController.java
+++ src/main/java/com/ebanx/demo/controller/EventController.java
@@ -1,9 +1,14 @@
 package com.ebanx.demo.controller;
 
+import com.ebanx.demo.model.dto.EventRequestDto;
+import com.ebanx.demo.model.dto.EventResponseDto;
 import com.ebanx.demo.service.EventService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +18,12 @@ public class EventController {
     EventService eventService;
 
     @PostMapping("/event")
-    public ResponseEntity<?> event() {
-        return ResponseEntity.ok("Event Controller");
+    public ResponseEntity<?> event(@RequestBody EventRequestDto eventRequestDto) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(eventService.process(eventRequestDto));
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(0);
+        }
     }
 
 }
